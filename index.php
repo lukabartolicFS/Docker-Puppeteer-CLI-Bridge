273d891013392fba429d3d482ac40e4ebb11d5ce
Luka Bartolic <lukabartolic@farshore.com>
2020-07-10 09:10:06 +0200

    Cleanup, minified Dockerized solution

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 $url = "https://dev.acarenet.com/webapp/userapi/memberdashboard/start/06-01-2020/end/07-02-2020/id/1774/tday/1/sur/0/cj/1/ce/1/key/f0bda020d2470f2e74990a07a607ebd9/selectedsurveylists/534,540,126,537/nam/Dashboard";
 
 // get the outputted binary file
-$fileBinary = shell_exec("node pdf_from_url.js {$url}");
+$fileBinary = shell_exec("node client.js {$url}");
 
 //file_put_contents('my.pdf', $fileBinary);
 
